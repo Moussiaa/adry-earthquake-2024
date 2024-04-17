5fdfb2ed270188d80ddfe77feb4114ee9cbe4bfa
Moussiaa <moussiaadry@gmail.com>
2024-04-17 01:11:42 -0400

    checkstyles correction

--- src/main/java/adry/earthquake/json/Geometry.java
+++ src/main/java/adry/earthquake/json/Geometry.java
@@ -6,9 +6,12 @@ public class Geometry {
     public List<Double> coordinates;
 
     public double getLongitude() {
+
         return coordinates.get(0);
     }
+
     public double getLatitude() {
+
         return coordinates.get(1);
     }
 }
